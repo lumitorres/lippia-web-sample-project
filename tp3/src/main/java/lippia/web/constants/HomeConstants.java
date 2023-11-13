be980a578b12434d672bd164ab93bf9642537a83
Ludmila Torres <ludmilatorresg@gmail.com>
2023-11-13 15:55:44 -0300

    Revert "repo fix"
    This reverts commit 3ff4e3bdb764dac00c95569cc5aebd6f22740285.

--- tp3/src/main/java/lippia/web/constants/HomeConstants.java
+++
@@ -1,8 +0,0 @@
-package lippia.web.constants;
-
-public class HomeConstants {
-    public static final String LOCATOR_SHOP_MENU = "xpath://*[@id=\"menu-item-40\"]/a";
-    public static final String LOCATOR_HOME_BUTTON = "xpath://*[@id=\"site-logo\"]/a/img" ;
-    public static final String LOCATOR_CONTAINER_SLIDER = "xpath://*[@id=\"n2-ss-6\"]/div[1]/div/div";
-    public static final String LOCATOR_CONTAINER_ARRIVALS = "xpath://*[@id=\"themify_builder_content-22\"]/div[2]/div/div/div/div/div[2]";
-}
--- tp3/src/main/java/lippia/web/constants/ShopConstants.java
+++
@@ -1,7 +0,0 @@
-package lippia.web.constants;
-
-public class ShopConstants {
-    public static final String LOCATOR_SHOP_MENU = "xpath://*[@id=\"menu-item-40\"]/a";
-    public static final String LOCATOR_SORTING_DROPDOWN = "xpath://*[@id=\"content\"]/form/select";
-}
-
