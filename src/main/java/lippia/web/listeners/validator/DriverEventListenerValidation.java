bbfb1f732a060422d2b30e304cc472c261313c05
Ludmila Torres <ludmilatorresg@gmail.com>
2023-11-13 15:47:45 -0300

    repo fix

--- src/main/java/lippia/web/listeners/validator/DriverEventListenerValidation.java
+++
@@ -1,8 +0,0 @@
-package lippia.web.listeners.validator;
-
-import org.openqa.selenium.WebDriver;
-
-public interface DriverEventListenerValidation {
-
-    void validate(Throwable throwable, WebDriver driver);
-}
